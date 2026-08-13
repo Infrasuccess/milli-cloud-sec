9606f3f4b653cecbfd27bec8df3dba3a1c5d9df0
Milli <guysbow@gmail.com>
2026-08-13 17:02:58 -0500

    Harden terraform apply pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "milli-cloud-sec-tf-state-419467346711"
-    key          = "devsec/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket         = "milli-cloud-sec-tf-state-419467346711"
+    key            = "devsec/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "milli-cloud-sec-tf-locks"
+    encrypt        = true
   }
 }
